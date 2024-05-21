8863ec464780ee82c47466554939e27254bbc42b
Leon Krüger <leonkrueger02@gmail.com>
2024-05-21 15:59:23 +0200

    modified evaluate_prompt_output

--- bachelorthesis/evaluate_prompt_output.py
+++ bachelorthesis/evaluate_prompt_output.py
@@ -20,7 +20,8 @@ HF_API_TOKEN = "YOUR_HF_API_TOKEN"
 
 # Switch if necessary
 strategy_name = "missing_tables_1500"
-evaluation_input_file = "table_not_in_database.json"
+fine_tuned_model_folder = "missing_tables_1500_1"
+evaluation_input_file = "table_not_in_database"
 evaluation_folder = os.path.join("further_evaluation", "error_cases_missing_tables")
 
 # Depends on if the script is run in Docker or as plain python
@@ -60,7 +61,7 @@ model = PeftModel.from_pretrained(
         os.path.dirname(os.path.realpath(__file__)),
         "fine_tuning",
         "output",
-        strategy_name,
+        fine_tuned_model_folder,
     ),
 )
 model = model.merge_and_unload()
@@ -135,43 +136,50 @@ def run_experiments_for_strategy(
 ) -> List[Dict[str, Any]]:
     result_points = []
     for data_point in evaluation_input:
-        # Run prompt directly
-        # prompt = generate_and_tokenize_prompt(data_point)
-        # data_point["predicted_table_name"] = run_prompt(prompt)
-        # result_points.append(data_point)
+        if evaluation_input_file == "different_name_in_database":
+            # Get different table name first
+            result_point = copy.deepcopy(data_point)
+            data_point["query"] = (
+                data_point["query"][:12]
+                + data_point["query"][data_point["query"].find("(") :]
+            )
+            table_name_generation_prompt = generate_and_tokenize_prompt(data_point)
 
-        # Get different table name first
-        result_point = copy.deepcopy(data_point)
-        data_point["query"] = (
-            data_point["query"][:12]
-            + data_point["query"][data_point["query"].find("(") :]
-        )
-        table_name_generation_prompt = generate_and_tokenize_prompt(data_point)
-        table_name = run_prompt(table_name_generation_prompt)
+            # Try 3 times. When no alternative name is found, skip this query.
+            for i in range(3):
+                table_name = run_prompt(table_name_generation_prompt)
+                if table_name not in data_point["database_state"].keys():
+                    break
+            if table_name in data_point["database_state"].keys():
+                continue
 
-        if table_name == data_point["expected_table_name"]:
-            continue
+            result_point["database_state"][table_name] = result_point[
+                "database_state"
+            ].pop(result_point["expected_table_name"])
+            result_point["expected_table_name"] = table_name
 
-        result_point["database_state"][table_name] = result_point["database_state"].pop(
-            result_point["expected_table_name"]
-        )
-        result_point["expected_table_name"] = table_name
-
-        # Run evaluation prompt
-        prompt = generate_and_tokenize_prompt(result_point)
-        result_point["predicted_table_name"] = run_prompt(prompt)
-        result_points.append(result_point)
+            # Run evaluation prompt
+            prompt = generate_and_tokenize_prompt(result_point)
+            result_point["predicted_table_name"] = run_prompt(prompt)
+            result_points.append(result_point)
+        else:
+            # Run prompt directly
+            prompt = generate_and_tokenize_prompt(data_point)
+            data_point["predicted_table_name"] = run_prompt(prompt)
+            result_points.append(data_point)
 
     return result_points
 
 
 with open(
-    os.path.join(evaluation_folder, evaluation_input_file), encoding="utf-8"
+    os.path.join(evaluation_folder, evaluation_input_file + ".json"), encoding="utf-8"
 ) as evaluation_file:
     evaluation_input = json.load(evaluation_file)
 
 output_folder = os.path.join(evaluation_folder, strategy_name)
-output_file_path = os.path.join(output_folder, "results_" + evaluation_input_file)
+output_file_path = os.path.join(
+    output_folder, "results_" + evaluation_input_file + ".json"
+)
 
 # Continue if experiment was already run
 with open(output_file_path, encoding="utf-8") as results_file:
