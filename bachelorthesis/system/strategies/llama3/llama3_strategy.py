1ac6c543f21c6fca93a10fabfadecba2f9d290d0
Leon Krüger <leonkrueger02@gmail.com>
2024-08-14 16:54:17 +0200

    Retry column mapping in llama3 when database column is predicted twice

--- bachelorthesis/system/strategies/llama3/llama3_strategy.py
+++ bachelorthesis/system/strategies/llama3/llama3_strategy.py
@@ -1,3 +1,4 @@
+import logging
 import re
 from copy import deepcopy
 from typing import Dict, List
@@ -12,10 +13,14 @@ class Llama3Strategy(Strategy):
         self,
         table_prediction_model_dir: str = None,
         column_mapping_model_dir: str = None,
+        max_column_mapping_retries: int = 5,
     ) -> None:
         self.model = Llama3Model()
+        self.logger = logging.getLogger(__name__)
+
         self.table_prediction_model_dir = table_prediction_model_dir
         self.column_mapping_model_dir = column_mapping_model_dir
+        self.max_column_mapping_retries = max_column_mapping_retries
 
     def predict_table_name(self, query_data: QueryData) -> str:
         # Loads the correct adapter for the table prediction task
@@ -84,37 +89,59 @@ class Llama3Strategy(Strategy):
                 else "No column specified"
             )
 
-            predicted = self.model.run_prompt(
-                [
-                    {
-                        "role": "system",
-                        "content": (
-                            "You are an intelligent database that predicts the columns of a SQL-insert. "
-                            "The inserts can contain abbreviated or synonymous column names. The column names can also be missing entirely. "
-                            "Base your guess on the available information. "
-                            "If there is a suitable column in the table answer its name. Else, predict a suitable name for a new column in this table. "
-                            "Answer only with the name of the column. Don't give any explanation for your result."
-                        ),
-                    },
-                    {
-                        "role": "user",
-                        "content": (
-                            "Predict the column for this value:\n"
-                            f"Query: {query_data.get_query(use_quotes=False)}\n"
-                            f"Specified column: {query_column}\n"
-                            f"Value: {query_value}\n"
-                            f"{table_string}\n"
-                            "Column:"
-                        ),
-                    },
-                ],
-            )
-            predicted_columns.append(predicted)
+            # Different columns from the query must not be mapped to the same database column
+            # If that is the case retry as long as specified
+            # If not one generated name fits, the last prediction is used and an integer is added to its end
+            was_added = False
+            for i in range(self.max_column_mapping_retries):
+                prediction = self.model.run_prompt(
+                    [
+                        {
+                            "role": "system",
+                            "content": (
+                                "You are an intelligent database that predicts the columns of a SQL-insert. "
+                                "The inserts can contain abbreviated or synonymous column names. The column names can also be missing entirely. "
+                                "Base your guess on the available information. "
+                                "If there is a suitable column in the table answer its name. Else, predict a suitable name for a new column in this table. "
+                                "Answer only with the name of the column. Don't give any explanation for your result."
+                            ),
+                        },
+                        {
+                            "role": "user",
+                            "content": (
+                                "Predict the column for this value:\n"
+                                f"Query: {query_data.get_query(use_quotes=False)}\n"
+                                f"Specified column: {query_column}\n"
+                                f"Value: {query_value}\n"
+                                f"{table_string}\n"
+                                "Column:"
+                            ),
+                        },
+                    ],
+                )
+
+                if prediction not in predicted_columns:
+                    predicted_columns.append(prediction)
+                    was_added = True
+                    break
+
+            # Modify prediction and add if no fitting name was found
+            if not was_added:
+                modification = 1
+                while (
+                    modified_prediction := prediction + modification
+                ) in predicted_columns:
+                    modification += 1
+                predicted_columns.append(modified_prediction)
+                self.logger.info(
+                    f"No column found for column: {query_column}, value: {query_value} in query: {query_data.get_query()}."
+                    f"Used column {modified_prediction} instead."
+                )
 
             # Changes table state, so that predicted column is no longer included
-            if predicted in db_columns:
-                column_index = db_columns.index(predicted)
-                db_columns.remove(predicted)
+            if prediction in db_columns:
+                column_index = db_columns.index(prediction)
+                db_columns.remove(prediction)
                 db_values = [
                     [value for i, value in enumerate(row) if i != column_index]
                     for row in db_values
