32d5353863f02ad82bbeea9b0fb954dbbbd6ddd7
Leon Krüger <leonkrueger02@gmail.com>
2024-12-18 14:08:42 +0100

    Improved synonym generation script

--- bachelorthesis/synoynm_generation.py
+++ bachelorthesis/synoynm_generation.py
@@ -1,6 +1,7 @@
 import json
 import os
 from collections import defaultdict
+from pathlib import Path
 
 from system.utils.utils import load_env_variables, remove_quotes
 
@@ -9,10 +10,26 @@ load_env_variables()
 from system.strategies.llama3.llama3_model import Llama3Model
 from tqdm import tqdm
 
+synonym_generation_data_path = (
+    Path(__file__).resolve().parent
+    / os.environ["EVALUATION_BASE_DIR"]
+    / "fine_tuning"
+    / "column_synonym_generation_data.json"
+)
+
+synonyms_path = (
+    Path(__file__).resolve().parent
+    / os.environ["EVALUATION_BASE_DIR"]
+    / "fine_tuning"
+    / "column_synonyms.json"
+)
+
 model = Llama3Model()
 
 
-def generate_synonyms(data_point: dict[str, str], synonyms) -> None:
+def generate_table_synonyms(
+    data_point: dict[str, str], synonyms: dict[str, dict[str, list[str]]]
+) -> None:
     if len(data_point["query"]) == 0:
         return
 
@@ -43,36 +60,60 @@ def generate_synonyms(data_point: dict[str, str], synonyms) -> None:
             )
 
 
-with open(
-    os.path.join(
-        os.path.dirname(os.path.realpath(__file__)),
-        "..",
-        "..",
-        "evaluation",
-        "bachelorthesis",
-        "fine_tuning",
-        "synonym_generation_data.json",
-    ),
-    encoding="utf-8",
-) as synonym_generation_data_file:
-    synonym_generation_data = json.load(synonym_generation_data_file)
+def generate_column_synonyms(
+    data_point: dict[str, str], synonyms: dict[str, dict[str, dict[str, list[str]]]]
+) -> None:
+    if len(data_point["query"]) == 0:
+        return
 
-synonyms = defaultdict(lambda: defaultdict(lambda: []))
+    for column in data_point["column_names"]:
+        messages = [
+            {
+                "role": "system",
+                "content": "Given three SQL insert query, you should predict a different name for the specified database column. "
+                "Answer only with the different name of the column. Don't give any explanation for your result.",
+            },
+            {
+                "role": "user",
+                "content": f"Predict a different name for the database column '{column}' for these insert queries.\n"
+                f"Query: {''.join(data_point['query'])}\n"
+                "Table:",
+            },
+        ]
 
-for data_point in tqdm(synonym_generation_data):
-    generate_synonyms(data_point, synonyms)
+        for i in range(3):
+            predicted_name = remove_quotes(model.run_prompt(messages))
 
-with open(
-    os.path.join(
-        os.path.dirname(os.path.realpath(__file__)),
-        "..",
-        "..",
-        "evaluation",
-        "bachelorthesis",
-        "fine_tuning",
-        "synonyms.json",
-    ),
-    mode="w",
-    encoding="utf-8",
-) as synonyms_file:
-    json.dump(synonyms, synonyms_file)
+            if (
+                predicted_name != column
+                and predicted_name
+                not in synonyms[data_point["database_name"]][data_point["table_name"]][
+                    column
+                ]
+            ):
+                synonyms[data_point["database_name"]][data_point["table_name"]][
+                    column
+                ].append(predicted_name)
+
+
+if __name__ == "__main__":
+    with open(
+        synonym_generation_data_path,
+        encoding="utf-8",
+    ) as synonym_generation_data_file:
+        synonym_generation_data = json.load(synonym_generation_data_file)
+
+    for data_point in tqdm(synonym_generation_data):
+        if "column" in synonym_generation_data_path:
+            synonyms = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: [])))
+            generate_column_synonyms(data_point, synonyms)
+        else:
+            synonyms = defaultdict(lambda: defaultdict(lambda: []))
+            generate_table_synonyms(data_point, synonyms)
+
+    with open(
+        synonyms_path,
+        mode="w",
+        encoding="utf-8",
+    ) as synonyms_file:
+        json.dump(synonyms, synonyms_file)
